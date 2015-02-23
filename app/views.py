99bb99ae6576321e471abc5823f938e24a5ed099
rolledback <matthew.rayermann@gmail.com>
2015-02-22 18:21:13 -0600

    Can now view player cache from web.

--- app/views.py
+++ app/views.py
@@ -16,18 +16,26 @@ def index():
     user = {'nickname': 'Matthew'}
     return render_template('index.html', title = 'Home', user = user)
 
+# view current vehicle IDs
 @app.route('/vehicles', methods = ['GET'])
 def get_veh_ids():
     return str(vehicle_ids)
 
+# view player ID cache
+@app.route('/cache', methods = ['GET'])
+def get_id_cache():
+    return str(id_cache)
+
 @app.route('/player/<name>', methods = ['GET'])
 def get_player(name):
+    # see if player id is in cache, if not then request via Wargaming API
     if(name not in id_cache):
         request = requests.get(base_url + 'account/list/?application_id=' + app_id + '&search=' + name)
         id_data = request.json()['data']
         id_cache[name] = str(id_data[0]['account_id'])
     user_id = id_cache[name]
 
+    # create JSON object with battle/win rate stats for the player's tanks
     player_tanks = {}
     request = requests.get(base_url + 'account/tanks/?application_id=' + app_id + '&account_id=' + user_id)
     tank_data = request.json()['data'][user_id]
@@ -35,13 +43,13 @@ def get_player(name):
     for tank in tank_data:
         tank_win_rate = (tank['statistics']['wins'] * 1.0 / tank['statistics']['battles']) * 100
         tank_battles = tank['statistics']['battles']
-
         stats_record = {'win_rate': str(tank_win_rate),
                         'battles': str(tank_battles)}
         player_tanks[vehicle_ids[str(tank['tank_id'])]] = stats_record
 
     return str(player_tanks)
 
+# load vehicle ID information from Wargaming API
 def load_vehicles():
     request = requests.get(base_url + 'encyclopedia/tanks/?application_id=' + app_id + '&fields=tank_id,short_name_i18n')
     data = request.json()['data']
